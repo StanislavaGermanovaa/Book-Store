6861c9996d357107b06cc60fdbec533fcf798b8e
Stanislava Germanova <114156419+StanislavaGermanovaa@users.noreply.github.com>
2024-08-05 17:35:22 +0300

    Delete Category

--- BookStore/src/main/java/bg/softuni/bookstore/model/enums/CategoryType.java
+++
@@ -1,7 +0,0 @@
-//package bg.softuni.bookstore.model.enums;
-//
-//public enum CategoryType {
-//    CRIME_STORY, HORROR, THRILLER, CLASSIC, LOVE_STORY, SCIENCE_FICTION, FANTASY, FAIRY_TALE;
-//
-//
-//}
